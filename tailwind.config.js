d5e519d300fae1c093699288816380365504cfbb
Super <tienthanhapp278@gmail.com>
2025-05-05 14:27:40 +0700

    5/5 afternoon

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,30 @@ module.exports = {
       spacing: {
         '128': '32rem',
       },
+      keyframes: {
+        'fade-in-up': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(20px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+        }
+      },
+      animation: {
+        'fade-in-up': 'fade-in-up 1s ease-out forwards'
+      },
+      animationDelay: {
+        '0s': '0s',
+        '1s': '1s',
+        '2s': '2s',
+        '3s': '3s',
+      }
     },
   },
-  plugins: [],
-} 
+  plugins: [
+    require('tailwindcss-animation-delay'),
+  ],
+}
