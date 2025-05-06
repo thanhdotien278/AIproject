d6dcf90e4d2eb860009f064cffd968b2de77356f
Super <tienthanhapp278@gmail.com>
2025-05-06 16:58:17 +0700

    Let icon blend in textbox

--- backend/server.js
+++ backend/server.js
@@ -68,7 +68,7 @@ app.get('/api/qrcode', async (req, res) => {
     // Build the registration URL
     const baseUrl = `${req.protocol}://${req.get('host')}`;
     let registrationUrl = `${baseUrl}/register`;
-    registrationUrl = `http://192.168.1.114:3000/register`;
+    registrationUrl = `http://192.168.1.111:3000/register`;
     // Add the conference code if provided
     if (code) {
       registrationUrl += `?code=${code}`;
